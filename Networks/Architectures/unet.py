c36f916f0cc3fbab2c582fb270782af7cfd7e339
uguraybek <160721662+uguraybek@users.noreply.github.com>
2025-12-09 15:50:23 +0100

    Add files via upload
    train on rgb channels and predicting rgb values

--- Networks/Architectures/unet.py
+++ Networks/Architectures/unet.py
@@ -17,11 +17,11 @@ class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.block = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False, padding_mode='reflect'),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
 
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False, padding_mode='reflect'),
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
         )
